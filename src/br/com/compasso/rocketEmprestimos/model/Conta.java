c206027d6e6e2cb49cc7708c7cc07e8b645ba2de
Víctor Florence de Quadros <m102139@ACAD.UNISC.BR>
2020-02-26 17:28:56 -0300

    PopulaBanco adicionado

--- src/br/com/compasso/rocketEmprestimos/model/Conta.java
+++ src/br/com/compasso/rocketEmprestimos/model/Conta.java
@@ -10,15 +10,40 @@ import javax.persistence.OneToOne;
 
 @Entity
 public class Conta {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@OneToOne
 	@JoinColumn(unique = true)
 	private Cliente cliente;
-	
+
 	@ManyToOne
 	private Agencia agencia;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Agencia getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(Agencia agencia) {
+		this.agencia = agencia;
+	}
+
 }
--- src/br/com/compasso/rocketEmprestimos/util/PopulaBanco.java
+++ src/br/com/compasso/rocketEmprestimos/util/PopulaBanco.java
@@ -2,7 +2,9 @@ package br.com.compasso.rocketEmprestimos.util;
 
 import javax.persistence.EntityManager;
 
+import br.com.compasso.rocketEmprestimos.model.Agencia;
 import br.com.compasso.rocketEmprestimos.model.Banco;
+import br.com.compasso.rocketEmprestimos.model.Conta;
 
 public class PopulaBanco {
 	
@@ -23,6 +25,45 @@ public class PopulaBanco {
 		banco3.setNome("Itaú");
 		banco4.setNome("Inter");
 		banco5.setNome("Santander");
+		
+		Agencia agencia1 = new Agencia();
+		Agencia agencia2 = new Agencia();
+		Agencia agencia3 = new Agencia();
+		Agencia agencia4 = new Agencia();
+		Agencia agencia5 = new Agencia();
+		
+		agencia1.setBanco(banco1);
+		agencia1.setNumero(2026);
+		
+		agencia2.setBanco(banco2);
+		agencia2.setNumero(1053);
+		
+		agencia3.setBanco(banco3);
+		agencia3.setNumero(1315);
+		
+		agencia4.setBanco(banco4);
+		agencia4.setNumero(4645);
+		
+		agencia5.setBanco(banco5);
+		agencia5.setNumero(5862);
+		
+		Conta conta1 = new Conta();
+		Conta conta2 = new Conta();
+		Conta conta3 = new Conta();
+		Conta conta4 = new Conta();
+		Conta conta5 = new Conta();
+		
+		conta1.setAgencia(agencia5);
+		conta2.setAgencia(agencia4);
+		conta3.setAgencia(agencia3);
+		conta4.setAgencia(agencia2);
+		conta5.setAgencia(agencia1);
+		
+		
+		
+		
+		
+		
 		
 		
 		
