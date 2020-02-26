c89ba8893732ddff2fc662c9e6f3fe2c293c9ebc
Sandro98 <sandronyland98@gmail.com>
2020-02-26 15:11:46 -0300

    Adicionando Agencia e Banco

---
+++ src/br/com/compasso/rocketEmprestimos/model/Agencia.java
@@ -0,0 +1,45 @@
+package br.com.compasso.rocketEmprestimos.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Agencia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private Integer numero;
+
+	@ManyToOne
+	private Banco banco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+
+}
