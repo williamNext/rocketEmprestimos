f7db856c1ecb5cabb84013f6543a563731b72212
Matheus Edison Barkert <m101589@ACAD.UNISC.BR>
2020-03-02 17:10:07 -0300

    nova classe ValidaLogin

---
+++ src/br/com/compasso/rocketEmprestimos/acao/ValidaLogin.java
@@ -0,0 +1,31 @@
+package br.com.compasso.rocketEmprestimos.acao;
+
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.compasso.rocketEmprestimos.dao.UsuarioDAO;
+import br.com.compasso.rocketEmprestimos.model.Usuario;
+
+public class ValidaLogin implements Acao{
+
+	@Override
+	public String executa(HttpServletRequest request, HttpServletResponse response) {
+		
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		EntityManager em = (EntityManager) request.getAttribute("EntityManager");
+		UsuarioDAO usuarioDAO = new UsuarioDAO(em);
+		
+		Usuario user = usuarioDAO.findByLoginAndSenha(login, senha);
+		
+		if (user != null) {
+			return "forward:FormCadastraEmprestimo.jsp";//revisar nao sei se esta certo
+		}else {
+			return "redirect:ValidaLogin";//revisar
+		}
+		
+	}
+
+}
